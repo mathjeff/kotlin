ce73b8b4d8434183e9a73dcd37eaca603c1492ee
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2021-09-01 14:57:31 +0300

    Change include <stdio.h> to <cstdio>

--- kotlin-native/runtime/src/main/cpp/Console.cpp
+++ kotlin-native/runtime/src/main/cpp/Console.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <stdio.h>
+#include <cstdio>
 
 #include "KAssert.h"
 #include "Memory.h"
